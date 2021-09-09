4eed59d873b5bd0d441cd3525df4e1a71771738a
oopsys6 <operasystem6@naver.com>
2021-09-09 23:47:05 +0900

    feat: 내장 디렉티브 적용

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-(function() {
-    var app = angular.module('todo', []);
+var app = angular.module('todo', []);
 
-    app.controller('TodoCtrl', ['$scope', function($scope) {
-        $scope.todo = {
-            title: '요가수련',
+app.controller('TodoCtrl', function($scope) {
+    $scope.todos = [
+        {
+            title: '요가 수행',
             completed: false,
             createdAt: Date.now()
+        },
+        {
+            title: '앵귤러 학습',
+            completed: false,
+            createdAt: Date.now()
+        },
+        {
+            title: '운동하기',
+            completed: true,
+            createdAt: Date.now()
         }
+    ];
 
-    }]);
-})();
+    $scope.remove = function(todo) {
+        var idx = $scope.todos.findIndex(function (item) {
+            return todo.title === item.title;
+        })
+
+        if (idx > -1) {
+            $scope.todos.splice(idx, 1)
+        }
+    }
+})
